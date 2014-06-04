69e8aa79d6c5bc02cd43f7f176225e896488f208
Andrew <xd_a5_2dance@hotmail.com>
2014-06-04 01:29:39 -0700

    added changes.html

---
+++ src/helmetModelException.java
@@ -0,0 +1,16 @@
+/**
+ * @author Andrew
+ *
+ */
+public class helmetModelException {
+
+	/**
+	 * @param args
+	 */
+	public helmetModelException() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
+
+
