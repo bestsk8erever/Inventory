5bef3cc6120cf253cbfb5cc15fc462643d93090b
andrew <xd_a5_2dance@hotmail.com>
2014-06-18 20:32:45 -0700

    wow MT

---
+++ src/ca/bcit/comp2613/a00832014/wow/DontHaveExpansionException.java
@@ -0,0 +1,13 @@
+package ca.bcit.comp2613.a00832014.wow;
+
+@SuppressWarnings("serial")
+public class DontHaveExpansionException extends Exception{
+	
+	public DontHaveExpansionException() {
+		super();
+	}
+
+	public DontHaveExpansionException(String message){
+		super(message);
+	}
+}
---
+++ src/ca/bcit/comp2613/a00832014/wow/Race.java
@@ -0,0 +1,7 @@
+package ca.bcit.comp2613.a00832014.wow;
+
+
+public enum Race {
+	WORGEN, DRAENEI, DWARF, GNOME, ORC, PANDAREN, HUMAN
+	// TODO, add a HUMAN Race
+}
