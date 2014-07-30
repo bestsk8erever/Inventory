d958183aaefa34f91f2cd34f4b788f355b92cd43
Andrew <xd_a5_2dance@hotmail.com>
2014-07-29 23:18:17 -0700

    pom update, insert fth

---
+++ src/ca/bcit/comp2613/a00832014/fth/CharacterRepository.java
@@ -0,0 +1,12 @@
+package ca.bcit.comp2613.a00832014.fth;
+
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+
+public interface CharacterRepository extends CrudRepository<Character, String> {
+
+}
---
+++ src/ca/bcit/comp2613/a00832014/fth/Race.java
@@ -0,0 +1,5 @@
+package ca.bcit.comp2613.a00832014.fth;
+
+public enum Race {
+	PANDAREN, BLOODELF, UNDEAD, GOBLIN, TROLL, ORC, TAUREN
+}
